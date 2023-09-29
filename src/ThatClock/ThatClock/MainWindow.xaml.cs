9d8b0860ebf8f31f190a55366b3c9081667662a9
Heiko Milke <hm@knm.io>
2023-09-29 10:25:56 +0200

    wip

---
+++ src/ThatClock/ThatClock/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ThatClock
+{
+    public partial class MainWindow
+    {
+        private DispatcherTimer timer = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            // initial call 
+            UpdateTime();
+
+
+            //RegisterMouseEvasion();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            CurrentTimeTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
---
+++ src/ThatClock/ThatClock/RegisterMouseEvasion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ThatClock;
+
+partial class MainWindow
+{
+    private bool isMouseOver = false;
+
+
+    private void RegisterMouseEvasion()
+    {
+        this.MouseMove += MainWindow_MouseMove;
+
+        this.MouseEnter += MainWindow_MouseEnter;
+        this.MouseLeave += MainWindow_MouseLeave;
+    }
+
+    private void MainWindow_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (isMouseOver)
+        {
+            Point mousePosition = e.GetPosition(this);
+
+            this.Left = mousePosition.X;
+            this.Top = mousePosition.Y;
+        }
+    }
+
+    private void MainWindow_MouseEnter(object sender, MouseEventArgs e)
+    {
+        isMouseOver = true;
+    }
+
+    private void MainWindow_MouseLeave(object sender, MouseEventArgs e)
+    {
+        isMouseOver = false;
+    }
+}
