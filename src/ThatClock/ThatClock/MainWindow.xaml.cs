2f38c8228299d7b4761ffd05340157f2a00775b7
Heiko Milke <hm@knm.io>
2023-09-30 15:02:10 +0200

    wip

--- src/ThatClock/ThatClock/MainWindow.xaml.cs
+++ src/ThatClock/ThatClock/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
@@ -7,19 +9,20 @@ namespace ThatClock
 {
     public partial class MainWindow
     {
-        private DispatcherTimer timer = new();
+        private readonly DispatcherTimer _timer = new();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
 
             // initial call 
             UpdateTime();
 
+            InitializeNotifyIcon();
 
             //RegisterMouseEvasion();
         }
@@ -38,6 +41,24 @@ namespace ThatClock
         private void UpdateTime()
         {
             CurrentTimeTextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Window_MouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ContextMenu contextMenu = new ContextMenu();
+
+            MenuItem menuItem = new MenuItem();
+            menuItem.Header = "Exit";
+            menuItem.Click += ExitItem_Click;
+
+            contextMenu.Items.Add(menuItem);
+
+            contextMenu.IsOpen = true;
+        }
+
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
---
+++ src/ThatClock/ThatClock/SystrayHandling.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+namespace ThatClock;
+
+partial class MainWindow
+{
+    private TaskbarIcon taskbarIcon;
+
+    private void InitializeNotifyIcon()
+    {
+        
+        ImageSource imageSource = LoadEmbeddedImage("media/clock.ico");
+
+        
+        
+        taskbarIcon = new TaskbarIcon();
+        taskbarIcon.IconSource = imageSource;
+        taskbarIcon.ToolTipText = "ThatClock";
+
+        // Fügen Sie ein Kontextmenü für das Symbol hinzu
+        ContextMenu contextMenu = new ContextMenu();
+
+        // MenuItem showMenuItem = new MenuItem();
+        // showMenuItem.Header = "Show";
+        // showMenuItem.Click += ShowMenuItem_Click;
+        // contextMenu.Items.Add(showMenuItem);
+
+        MenuItem exitMenuItem = new MenuItem();
+        exitMenuItem.Header = "Exit";
+        exitMenuItem.Click += ExitMenuItem_Click;
+
+        contextMenu.Items.Add(exitMenuItem);
+
+        taskbarIcon.ContextMenu = contextMenu;
+    }
+    
+    private ImageSource LoadEmbeddedImage(string imagePath)
+    {
+        try
+        {
+            Uri uri = new Uri("pack://application:,,,/" + imagePath);
+            return new BitmapImage(uri);
+        }
+        catch 
+        {
+            return null;
+        }
+    }
+
+    private void ShowMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        // Ihre App anzeigen
+        this.Show();
+        this.WindowState = WindowState.Normal;
+    }
+
+    private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+    {
+        // Ihre App beenden
+        Application.Current.Shutdown();
+    }
+
+    private void Window_StateChanged(object sender, EventArgs e)
+    {
+        // Minimieren Sie das Fenster in das Systray, wenn es minimiert wird
+        if (this.WindowState == WindowState.Minimized)
+        {
+            this.Hide();
+        }
+    }
+}
